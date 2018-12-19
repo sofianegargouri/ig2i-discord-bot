a1efe946a2ff4ac5103c6cfdb05f21bcb9dcef3b
Sofiane Gargouri <contact@sofianeg.com>
2018-12-19 09:12:36 +0100

    :sparkles: Adding rotating statuses

--- src/classes/bot.ts
+++ src/classes/bot.ts
@@ -1,11 +1,34 @@
 import discordJs, { Client } from 'discord.js';
+import { movies } from '../constants';
 
 export default class Bot {
   public client: Client;
+  private token: string | undefined;
+  private statusIndex: number = 0;
 
   constructor(token: string | undefined) {
     this.client = new discordJs.Client();
+    this.token = token;
 
-    this.client.login(token);
+    this.login();
+    this.setStatus();
+  }
+
+  login() {
+    this.client.login(this.token);
+  }
+
+  setStatus() {
+    setInterval(
+      () => {
+        if (this.statusIndex + 1 >= movies.length) {
+          this.statusIndex = 0;
+        } else {
+          this.statusIndex += 1;
+        }
+        this.client.user.setActivity(movies[this.statusIndex], { type: 'WATCHING' });
+      },
+      60000,
+    );
   }
 }
---
+++ src/constants/movies.ts
@@ -0,0 +1,8 @@
+export default [
+  'Mr Bean',
+  'Stranger Things',
+  'Police Squad',
+  'Sneaky Pete - Season 1',
+  'Captain Philips',
+  'Sneaky Pete - Season 2',
+];
